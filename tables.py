196100b79cb2fbfe4982e0f31f9a96ec4b6848cc
gorgick <gorgick2014@yandex.by>
2024-09-18 19:29:33 +0300

    tables

---
+++ tables.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class TaskOrm(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    description: Mapped[Optional[str]]
